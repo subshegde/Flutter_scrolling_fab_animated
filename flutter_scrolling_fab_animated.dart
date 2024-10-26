b778a9f61a1e269d49e8f7a6a19601e05b0de8c8
Subrahmanya S Hegde <125819438+subshegde@users.noreply.github.com>
2024-10-26 22:28:50 +0530

    Update flutter_scrolling_fab_animated.dart

--- flutter_scrolling_fab_animated.dart
+++ flutter_scrolling_fab_animated.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_scrolling_fab_animated/flutter_scrolling_fab_animated.dart';
 
+class ScrollingFabAnimatedExample extends StatefulWidget {
+  const ScrollingFabAnimatedExample({Key? key}) : super(key: key);
+
+  @override
+  _ScrollingFabAnimatedExampleState createState() => _ScrollingFabAnimatedExampleState();
+}
+
+class _ScrollingFabAnimatedExampleState extends State<ScrollingFabAnimatedExample> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter scrolling fab animated'),),
+      body:const Column(),
+      floatingActionButton: ScrollingFabAnimated(
+        width: 150,
+        icon: const Icon(Icons.add),
+        text: const Text('Add'),
+        color: Colors.blue,
+        onPress: () {
+        print('pressed');
+        },
+        onLongPress: (){
+          print('long pressed');
+        },
+        scrollController: _scrollController,
+        animateIcon: true,
+      ),
+    );
+  }
+}
